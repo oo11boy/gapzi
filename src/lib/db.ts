dee1f2e7e55c8f71e4b046e94984307d613f7184
oo11boy <editkhone@gmail.com>
2025-11-27 21:35:36 +0330

    fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,10 @@
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root', // username MySQL خودت
-  password: '', // password MySQL خودت
-  database: 'chat_system',
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "chat_system",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
